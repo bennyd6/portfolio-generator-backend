0351fe56c134d948d7658431e24a42915c45bf6d
bennyd6 <228r1a66d6@cmrec.ac.in>
2025-06-25 23:00:03 +0530

    test commit

--- gemini.js
+++ gemini.js
@@ -1,19 +1,19 @@
-import { GoogleGenAI } from "@google/genai";
-import dotenv from "dotenv";
+// import { GoogleGenAI } from "@google/genai";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
+// const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
 
-async function main() {
-  const result = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
-  });
+// async function main() {
+//   const result = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: "Explain how AI works in a few words",
+//   });
 
-  const response = await result.response;
-  const text = await response.text();
-  console.log(text);
-}
+//   const response = await result.response;
+//   const text = await response.text();
+//   console.log(text);
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
